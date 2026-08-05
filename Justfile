e62a332ddb0389ebbb278d79d7fc48d71db112d6
Charlotte Smith <charliesmith5019@gmail.com>
2026-08-05 10:55:48 +0100

    add justfile

---
+++ Justfile
@@ -0,0 +1,42 @@
+[private]
+default:
+    @just -l
+
+[working-directory: 'lib']
+build:
+    cargo build
+
+[working-directory: 'lib']
+build-release:
+    cargo build --release
+
+[working-directory: 'lib']
+test-release:
+    cargo nextest run --release
+
+[working-directory: 'lib']
+test:
+    cargo nextest run
+
+rust_output_dir := 'lib/target'
+debug_dir := rust_output_dir / 'debug'
+release_dir := rust_output_dir / 'release'
+output_dir := 'lua'
+output_name := 'hex_color_rs.so'
+output := output_dir / output_name
+
+[linux]
+_deploy dir:
+    cp {{dir / 'libhex_color_rs.so'}} {{output}}
+
+[macos]
+_deploy dir:
+    cp {{dir / 'libhex_color_rs.dylib'}} {{output}}
+
+[windows]
+_deploy dir:
+    copy {{dir / 'hex_color_rs.dll'}} {{output}}
+
+deploy-debug: test build (_deploy debug_dir)
+
+deploy: test-release build-release (_deploy release_dir)
--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,1 +1,38 @@
 mod color;
+
+use nvim_oxi::{Dictionary, Function, Result as NvimResult, api::Buffer};
+
+fn lines_to_string(lines: impl Iterator<Item = nvim_oxi::String>) -> String {
+    let mut s = String::new();
+    let mut first = true;
+    for l in lines {
+        if !first {
+            s.push('\n');
+        }
+        s.push_str(&l.to_string_lossy());
+        first = false;
+    }
+
+    s
+}
+
+fn highlight_hex_strings(bufnr: Option<i32>) -> NvimResult<()> {
+    let buf = bufnr
+        .map(Buffer::from)
+        .unwrap_or_else(nvim_oxi::api::get_current_buf);
+
+    let lines = buf.get_lines(.., false)?;
+    let s = lines_to_string(lines);
+
+    nvim_oxi::print!("hello {}", s);
+
+    Ok(())
+}
+
+#[nvim_oxi::plugin]
+fn hex_color_rs() -> Dictionary {
+    let highlight_hex_strings: Function<Option<i32>, NvimResult<()>> =
+        Function::from_fn(highlight_hex_strings);
+
+    Dictionary::from_iter([("highlight_hex_strings", highlight_hex_strings)])
+}
